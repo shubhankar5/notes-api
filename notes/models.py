a249a4767340eea4e969be32b8cf33cd56eafe98
Shubhankar <shubhankarbhagat@gmail.com>
2021-12-15 02:35:14 +0530

    Add models and serializers for note app

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Note(models.Model):
+	title = models.CharField('Title', max_length=100)
+	text = models.CharField('Text', max_length=1000, null=True, blank=True)
+	owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='notes', verbose_name='Owner')
+	created_on = models.DateField('Created On', default=date.today)
+
+	def __str__(self):
+		return self.title
+	
