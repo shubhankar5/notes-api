3a87d209b24657025451588b631c87caafe9554b
Shubhankar <mitronmanager@gmail.com>
2022-01-02 01:52:05 +0530

    Update throttling rates

--- notes/throttlers.py
+++ notes/throttlers.py
@@ -3,9 +3,9 @@ from rest_framework.throttling import UserRateThrottle
 
 class GetRateThrottle(UserRateThrottle):
 	scope = 'get'
-	rate = '5/day'
+	rate = '1000/day'
 
 
 class PostRateThrottle(UserRateThrottle):
 	scope = 'post'
-	rate = '1/day'
+	rate = '50/day'
