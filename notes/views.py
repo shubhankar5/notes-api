ff30d1e2d3e7ccaa8c05c52b3e6c25aaf803cfa2
Shubhankar <mitronmanager@gmail.com>
2022-01-02 01:13:55 +0530

    Update permission of the viewset

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Note
 from .serializers import NoteSerializer
 
@@ -6,3 +6,4 @@ from .serializers import NoteSerializer
 class NoteViewSet(viewsets.ModelViewSet):
 	queryset = Note.objects.all()
 	serializer_class = NoteSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
